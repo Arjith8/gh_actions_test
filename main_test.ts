52ac07a8e4c8d4f52e4b3390e48b2729f8629834
Arjithar8 <arjithar8@gmail.com>
2024-11-26 00:51:31 +0530

    ygygde

--- main_test.ts
+++ main_test.ts
@@ -4,3 +4,11 @@ import { add } from "./main.ts";
 Deno.test(function addTest() {
   assertEquals(add(2, 3), 5);
 });
+
+Deno.test(function test(){
+    assertEquals(add(3,3), 6);
+})
+
+Deno.test(function test(){
+    assertEquals(add(3,4), 8);
+})
