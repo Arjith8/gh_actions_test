7c8bfbb571a843c0fd27a4bd04dfc2298a79121d
Arjith A R <122336800+Arjith8@users.noreply.github.com>
2024-11-29 20:15:25 +0530

    Update deploy.py

--- .github/workflows/deploy.py
+++ .github/workflows/deploy.py
@@ -6,4 +6,5 @@ print(f"The value of MY_ENV_VAR is: {value}")
 
 # Access environment variables directly (throws a KeyError if not set)
 value_direct = os.environ['MY_ENV_VAR']
-print(f"The value of MY_ENV_VAR is:
+print(f"The value of MY_ENV_VAR is: {value_direct}")
+
